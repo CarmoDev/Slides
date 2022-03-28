e474e3c02778b8ed95eefb15421ef550bd17a962
Jefferson Santos <dalvatechnologies@gmail.com>
2022-03-28 18:17:41 -0300

    calculo de distancias e de velocidade do mouse

--- modulos/slide.js
+++ modulos/slide.js
@@ -2,19 +2,37 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.distance = {
+            finalPosition: 0,
+            startX: 0,
+            movement: 0,
+        }
+    }
+
+    moveSlide(distX) {
+        this.distance.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+    }
+
+    updatePosition(clientX) {
+        this.distance.movement = (this.distance.startX - clientX) * 1.6
+        return this.distance.finalPosition - this.distance.movement
     }
 
     onStart(Event) {
         Event.preventDefault();
+        this.distance.startX = Event.clientX;
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
     onMove(Event) {
-        console.log('moveu');
+        const finalPosition = this.updatePosition(Event.clientX)
+        this.moveSlide(finalPosition)
     }
 
     onEnd(Event) {
         this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.distance.finalPosition = this.distance.movePosition;
     }
 
     addSlideEvents() {
